0c24f31b6911bc022a7abf4b3f1b7112715c7f24
Graham Doerksen <graham.doerksen@attabotics.com>
2023-03-09 20:28:08 -0700

    Been working through rustlings the last few days

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,18 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
 // fn calculate_price_of_apples {
+
+pub fn calculate_price_of_apples(qty: u32)->u32{
+    if qty > 40 {
+        qty
+    } else {
+        let price = 2;
+        qty * price
+    }
+
+}
 
 // Don't modify this function!
 #[test]
